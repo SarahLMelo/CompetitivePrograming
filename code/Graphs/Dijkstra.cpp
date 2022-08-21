e686e7a0804d3f3aee809b4269e85c6544960d5c
SarahLMelo <61260788+SarahLMelo@users.noreply.github.com>
2022-08-21 20:54:34 -0300

    Update Dijkstra.cpp

--- code/Graphs/Dijkstra.cpp
+++ code/Graphs/Dijkstra.cpp
@@ -1,1 +1,43 @@
+#include <bits/stdc++.h>
 
+using namespace std;
+
+typedef long long ll;
+typedef pair <ll, ll> ii;
+const ll inf = 1e18;
+
+int n, m;
+vector <vector <ii>> g;
+vector <ll> dist;
+
+void init(){
+    g.resize(n);
+    dist.assign(n, inf);
+}
+
+void dijkstra(int x){
+    dist[x] = 0;
+
+    priority_queue <ii, vector <ii>, greater <ii>> pq;
+    pq.emplace(0, x);
+
+    while(!pq.empty()){
+        ll w, u;
+        w = pq.top().first;
+        u = pq.top(). second;
+
+        pq.pop();
+
+        if(dist[u] < w) continue;
+
+        for(auto i:g[u]){
+
+            if(dist[i.first] > w + i.second){
+                dist[i.first] = w + i.second;
+                pq.emplace(w + i.second, i.first);
+            }
+        }
+    }
+
+    return;
+}
