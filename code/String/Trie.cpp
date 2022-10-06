f9ac3b1145c9a69543f010c0cda2f487f67865bf
SarahLMelo <61260788+SarahLMelo@users.noreply.github.com>
2022-10-05 23:56:49 -0300

    Update Trie.cpp

--- code/String/Trie.cpp
+++ code/String/Trie.cpp
@@ -1,1 +1,28 @@
+/*
+Onde ms é a soma dos tamanhos das strings, sigma o tamanho do alfabeto.
+*/
+int trie[ms][sigma], terminal[ms], z = 1;
 
+void insert(string &p) {
+  int cur = 0;
+  for(int i = 0; i < p.size(); i++) {
+    int id = p[i]-'a';
+    if(!trie[cur][id]) {
+      trie[cur][id] = z++;
+    }
+    cur = trie[cur][id];
+  }
+  terminal[cur]++;
+}
+
+int count(string &p) {
+  int cur = 0;
+  for(int i = 0; i < p.size(); i++) {
+    int id = p[i]-'a';
+    if(!trie[cur][id]) {
+      return false;
+    }
+    cur = trie[cur][id];
+  }
+  return terminal[cur];
+}
