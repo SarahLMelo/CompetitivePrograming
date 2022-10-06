f8db425b4f1bc26ff9c7fc7faa73a659767d5196
SarahLMelo <61260788+SarahLMelo@users.noreply.github.com>
2022-10-06 13:10:47 -0300

    Create Kruskal.cpp

---
+++ code/Graphs/Kruskal.cpp
@@ -0,0 +1,21 @@
+struct Edge{
+	int u, v, cost;
+	bool operator < (Edge b) {
+		return cost < b.cost;
+	}
+};
+void Kruskal() {
+	vector<Edge> edge(m);
+	for (int i = 0; i < m; i++) {
+		cin >> edge[i].u >> edge[i].v >> edge[i].cost;
+	}
+	sort(edge.begin(), edge.end());
+	long long total = 0;
+	for (int i = 0; i < m; i++) {
+		if (find(edge[i].u) != find(edge[i].v)) {
+			merge(edge[i].u, edge[i].v);
+			mst.push_back(edge[i]);
+      total += edge[i].cost;
+		}
+	}
+}
