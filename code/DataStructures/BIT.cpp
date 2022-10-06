b48e8020e8553db2ee3e956fee6c26134c4f0ce5
SarahLMelo <61260788+SarahLMelo@users.noreply.github.com>
2022-10-06 00:17:06 -0300

    Update BIT.cpp

--- code/DataStructures/BIT.cpp
+++ code/DataStructures/BIT.cpp
@@ -1,1 +1,17 @@
+int bit[ms], n;
 
+void update(int idx, int v) {
+  while(idx <= n) {
+    bit[idx] += v;
+    idx += idx & -idx;
+  }
+}
+
+int query(int idx) {
+  int ans = 0;
+  while(idx > 0) {
+    ans += bit[idx];
+    idx -= idx & -idx;
+  }
+  return ans;
+}
